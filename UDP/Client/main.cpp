0a833c8903ab621e7e21588ec2ac59a063d190e9
Tim <sterzeltim@gmail.com>
2016-08-28 18:54:42 +0200

    Implementing UDP sending and receiving

--- UDP/Client/main.cpp
+++ UDP/Client/main.cpp
@@ -4,10 +4,23 @@
 
 // UDP connection test
 int main()
-{
+{        
 	std::cout << "Client" << std::endl;
+	std::cout << "IP ?" << std::endl;
 	std::string ip;
 	std::cin >> ip;
+	sf::UdpSocket socket;
+	unsigned short port = 54000;
+
+	char data[11] = { 'H', 'E', 'L', 'L', 'O', ' ', 'W', 'O', 'R', 'L', 'D' };
+	if (socket.send(data, sizeof(data), ip, port) != sf::Socket::Done)
+	{
+		std::cerr << "Error by sending data" << std::endl;
+	}
+	else
+	{
+		std::cout << "Sended data successfully" << std::endl;
+	}
 }
 
 
--- UDP/Server/main.cpp
+++ UDP/Server/main.cpp
@@ -1,8 +1,38 @@
 #include <SFML/Network.hpp>
 #include <iostream>
+#include <string>
 
 // UDP connection test 
 int main()
 {
 	std::cout << "Server" << std::endl;
+	
+	unsigned short port = { 54000 };
+	sf::UdpSocket socket;
+	if (socket.bind(port) != sf::Socket::Done)
+	{
+		std::cerr << "Error by binding socket" << std::endl;
+	}
+	else
+	{
+		std::cout << "Binding successfully" << std::endl;
+	}
+	
+	char data[11];
+	sf::IpAddress senderIp;
+	std::size_t received;
+	if (socket.receive(data, sizeof(data), received, senderIp, port) != sf::Socket::Done)
+	{
+		std::cerr << "Error by receiving data" << std::endl; 
+	}
+	else
+	{
+		std::cout << "Received: " << received << " bytes from " << senderIp << " on port " << port << std::endl;
+		std::cout << "Message:" << std::endl;
+		for (char c : data)
+		{
+			std::cout << c;
+		}
+		std::cout << std::endl;
+	}
 }
